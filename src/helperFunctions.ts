f3eeb2c0f8db96e02f55a1cf57c1c202f5150d1d
immedha <immedha@outlook.com>
2025-02-04 08:56:14 -0800

    redeployed - works but has an issue with cursor messing up when you type while something is being generated

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -1,28 +1,28 @@
-export const generateWords = async (input: string, systemPrompt: string, cursorPos: number) => {
-  try {
-    await new Promise(resolve => setTimeout(resolve, 400));
-    return "This is a hardcoded string";
-  } catch (error) {
-    console.error("Error generating words:", error);
-    return "";
-  }
-};
-
-
-
 // export const generateWords = async (input: string, systemPrompt: string, cursorPos: number) => {
 //   try {
-//     const response = await fetch("https://us-central1-ai-auto-complete-a3a36.cloudfunctions.net/generateWords", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ input, systemPrompt, cursorPos }),
-//     });
-
-//     const data = await response.json();
-//     return data.generatedText || "";
-//     // return "HEY HEY HEY";
+//     await new Promise(resolve => setTimeout(resolve, 400));
+//     return "This is a hardcoded string";
 //   } catch (error) {
 //     console.error("Error generating words:", error);
 //     return "";
 //   }
 // };
+
+
+
+export const generateWords = async (input: string, systemPrompt: string, cursorPos: number) => {
+  try {
+    const response = await fetch("https://us-central1-ai-auto-complete-a3a36.cloudfunctions.net/generateWords", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ input, systemPrompt, cursorPos }),
+    });
+
+    const data = await response.json();
+    return data.generatedText || "";
+    // return "HEY HEY HEY";
+  } catch (error) {
+    console.error("Error generating words:", error);
+    return "";
+  }
+};
